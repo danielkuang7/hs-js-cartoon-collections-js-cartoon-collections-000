1b59be10483fa06614465c3759c24fb420112147
Shu Chao /Daniel Kuang <danielkuang7@gmail.com>
2018-12-13 21:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 function dwarfRollCall(dwarves) {
+  var newStr = "";
+  dwarves.forEach(function(name, idx) {
+  	newStr += (idx+1 + ". " + name + " ");
+  });
+  return newStr;
+}
+function dwarfRollCall(dwarves) {
+	var newStr = "";
+	for(var i = 0; i < dwarves.length; i++) {
+		if(i === dwarves.length-1) {
+			newStr += (i+1 + ". " + dwarves[i]);
+		}else {
+			newStr += (i+1 + ". " + dwarves[i] + " ");
+		}
+	}
+	return newStr;
+}
+
+function dwarfRollCall(dwarves) {
+	var newStr = [];
+	dwarves.forEach(function(name, i) {
+		newStr.push(i+1 +". " + name);
+	});
+	return newStr.join(" ");
 }
 
 function summonCaptainPlanet(planeteerCalls){

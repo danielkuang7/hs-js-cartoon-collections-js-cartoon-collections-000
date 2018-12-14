5989682d98f179d5c96f3d9e067c5b0f5d31dfd5
Shu Chao /Daniel Kuang <danielkuang7@gmail.com>
2018-12-14 00:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function dwarfRollCall(dwarves) {
+     var array =[]
+     for (var i=0;i<dwarves.length;i++){
+       array.push(`${i+1}. ${dwarves[i]} `)
+     }
+     return array.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(function(ele) {
+		return ele.toUpperCase() + "!";
+	});
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function findTheCheese (foods) {
+  var cheese = ["camembert", "cheddar", "gouda"];
+	for(var i = 0; i < foods.length; i++) {
+		var cheeseIdx = cheese.indexOf(foods[i]);
+		if(cheeseIdx !== -1) {
+			return foods[i];
+		}
+	}
+	return "no cheese!";
 }
